c5037b65f6dbd9db8a929833cfa950a42590c3e0
Lavaman491 <79179305+Lavaman491@users.noreply.github.com>
2023-11-17 14:40:54 -0500

    Create message.js
    Centralized messages

---
+++ message.js
@@ -0,0 +1,25 @@
+function getMessage() {
+	var i = Math.floor(r_text.length * Math.random());
+	let thing = document.getElementById("hi");
+	thing.innerHTML = messages[i];
+};
+
+array messages = [
+	"I know where you sleep",
+	"Im in your walls",
+	"I will find you",
+	"Check your closet before you sleep tonight",
+	"Youre not suppose to be here",
+	"Open the door, its cold in here",
+	"Its always watching",
+	"Your skin looks uncomfy, peel it off",
+	"Your bones need to breathe, take them out",
+	"Help me..",
+	"Its cold down here",
+	"...",
+	"I can see you",
+	"The bugs, theyre in your skin",
+	"Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy. Crazy?",
+	"Youre Gay"
+  ]
+
