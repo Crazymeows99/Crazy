90e3edf019ce99a7b3000ef4cfa029fb0c935cf1
Lavaman491 <79179305+Lavaman491@users.noreply.github.com>
2024-03-01 08:51:15 -0500

    Add files via upload

---
+++ dmElements.js
@@ -0,0 +1,142 @@
+
+/*
+Example:  
+const dmElements = {
+	"#idNameHere": "dmClassHere",
+	".classNameHere": "dmClassHere"
+}
+*/
+
+const dmElements = {
+	"#JamesBox": "dark-mode1",
+	"#purplebox": "dark-mode2",
+	
+	
+	".border": "dark-mode3",
+}
+
+
+
+
+/* To Convert:
+
+	
+	element = document.getElementById("purplebox");						--- done
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebox2");					--- done
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebox3");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebox4");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebox5");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebox6");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebutton");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purplebutton1");
+	element.classList.toggle("dark-mode2");
+	
+	element = document.getElementById("purpleborder");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder1");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder2");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder3");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder4");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder5");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder6");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder7");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder8");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder9");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder10");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder11");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("purpleborder12");
+	element.classList.toggle("dark-mode3");
+	
+	element = document.getElementById("jamessays");
+	element.classList.toggle("dark-mode4");
+	
+	element = document.getElementById("hi");
+	element.classList.toggle("dark-mode4");
+	
+	element = document.getElementById('ClTh');
+	if (darkmode) {
+		element.src='pics/ClThDm.png';
+		alert("Dark Mode Not Finished")
+	} else {
+		element.src='pics/Class Titlehead New.png';
+	}
+	
+	element = document.getElementById('H');
+	if (darkmode) {
+		element.src='pics/HDm.png';
+	} else {
+		element.src='pics/Fixed Heart.png';
+	}
+	
+	element = document.getElementById('GalTh');
+	if (darkmode) {
+		element.src='pics/GalThDm.png';
+	} else {
+		element.src='pics/Gallery Titlehead.png';
+	}
+	
+	element = document.getElementById('MTFTh');
+	if (darkmode) {
+		element.src='pics/MTFThDm.png';
+	} else {
+		element.src='pics/Meet the Founders Titlehead.png';
+	}
+	
+	
+	const element1 = document.getElementByClassName("border");				---- done
+	element.classList.toggle("dark-mode3");
+	
+	element1 = document.getElementByClassName("dropdown");
+	element.classList.toggle("dark-mode2");
+	
+	element1 = document.getElementByClassName("dropdown-content");
+	element.classList.toggle("dark-mode2");
+	
+	element1 = document.getElementByClassName("dropdown-content a");
+	element.classList.toggle("dark-mode2");
+	
+	element1 = document.getElementByClassName("dropdown-content a:hover");
+	element.classList.toggle("dark-mode2");
+	
+	element1 = document.getElementByClassName("dropdown:hover .dropdown-content");
+	element.classList.toggle("dark-mode2");
+	
+*/
+
