bcb1b33a54991ef670c0c5f5b898c669c9667575
Lavaman491 <79179305+Lavaman491@users.noreply.github.com>
2024-03-15 10:08:43 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -20,7 +20,6 @@ function searchLocal() {
 	else {
 		darkmode = false;
 		localStorage.setItem("darkmode", false);
-		console.log(localStorage.getItem("admin"));
 	}
 	if (darkmode) {
 		myFunction(doToggle=false);
